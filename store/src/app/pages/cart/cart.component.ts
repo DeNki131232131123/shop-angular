9604f5b6383fbb03c1bfc5d17c1a4d6d06e5e4b7
DeNki131232131123 <49789128+DeNki131232131123@users.noreply.github.com>
2022-11-21 13:59:50 +0100

    update_21.11

--- store/src/app/pages/cart/cart.component.ts
+++ store/src/app/pages/cart/cart.component.ts
@@ -26,13 +26,6 @@ export class CartComponent implements OnInit {
     product: 'https://via.placeholder.com/150',
     name: 'snickers',
     price: 150,
-    quantity: 3,
-    id: 3
-  },
-  {
-    product: 'https://via.placeholder.com/150',
-    name: 'snickers',
-    price: 150,
     quantity: 1,
     id: 4
   }]};
@@ -48,10 +41,26 @@ export class CartComponent implements OnInit {
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) =>{
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
   getTotal(items: Array<CartItem>): number {
     return this.cartService.getTotal(items);
-  }
 
+  }
+  onClearCart():void{
+    this.cartService.clearCart();
+  }
+  onRemoveFromCart(item: CartItem):void{
+    this.cartService.removeFromCart(item);
+
+  }
+  onAddQuantity(item: CartItem): void{
+    this.cartService.addToCart(item);
+  }
+  onRemoveQuantity(item: CartItem): void{
+    this.cartService.removeQuantity(item)
+  }
 }
